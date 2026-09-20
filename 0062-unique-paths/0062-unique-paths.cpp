a7ab9637c4d0a950173c5811213798cefdd0743a
Jetash jethi <jetashjethi@gmail.com>
2026-09-20 09:21:44 +0530

    Time: 2 ms (20.98%), Space: 9.3 MB (68.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int unique(int row,int col,vector<vector<int>>&dp){
-        if(row<0||col<0){return 0;}
-        if(row==0&&col==0){return 1;}
-        if(dp[row][col]!=-1){return dp[row][col];}
-        int row_minus=unique(row-1,col,dp);
-        int col_minus=unique(row,col-1,dp);
-        return dp[row][col]=row_minus+col_minus;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return unique(m-1,n-1,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i-1>=0){dp[i][j]+=dp[i-1][j];}
+                if(j-1>=0){dp[i][j]+=dp[i][j-1];}
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
