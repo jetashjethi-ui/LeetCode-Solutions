6739d00826aed84fe5e351a42d35d3f064d4ec95
Jetash jethi <jetashjethi@gmail.com>
2026-09-20 16:27:00 +0530

    Time: 0 ms (100%), Space: 13.4 MB (44.28%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=triangle[n-1][i];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int diag;
+                int down=dp[i+1][j]+triangle[i][j];
+                if(j+1<=n-1){diag=dp[i+1][j+1]+triangle[i][j];}
+                dp[i][j]=min(diag,down);
+            }
+        }
+        return dp[0][0];
+    }
+};
